a7a535563a7c6d9c228c6f27357605e419b5f4da
GreciaDCastillo <grecia.castillo@ucb.edu.bo>
2024-12-12 20:58:12 -0400

    si.

--- TendenciaTotal.cpp
+++ TendenciaTotal.cpp
@@ -30,17 +30,32 @@ struct historial_compras { //para archivo de texto
     int fecha; //dia
 };
 
-struct ropa {
+struct catalogo_ropa {
     string codigo_ropa;
-    enum categoria { deportiva, casual, formal, urbana, accesorios } categoria_ropa;
+    string categoria;
+    string marca;
+    string talla;
+    double precio_unitario;
+    int cantidad; // Cantidad disponible en el catálogo.
+    string temporada;
+};
+
+struct inventario_ropa {
+    string codigo_ropa;
+    string categoria;
     string marca;
     string talla;
     double costo_unitario;
-    double precio_unitario;
-    int cantidad;
-    enum temporada { verano, otono, invierno, primavera } temporada_ropa;
+    int cantidad; // Cantidad en el inventario general.
+    string temporada;
 };
 
+
+struct pedidos { //para archivo de texto
+    string codigo_pedido; //codigo ropa
+    int cantidad;
+    double total;
+};
 
 struct evento { // se tiene que hacer estrura de esto?
     string tipo;
@@ -50,8 +65,10 @@ struct evento { // se tiene que hacer estrura de esto?
 };
 vector<empleado> registro_empleado;
 vector<cliente> clientes;
-vector<ropa> catalogo;
+vector<catalogo_ropa> catalogo;
+vector<inventario_ropa> inventario;
 vector <evento> eventos;
+vector <pedidos> ventas;
 
 // Declaración de funciones
 void inicioSesionEmpleado(vector<empleado> &registro_empleado);
@@ -60,6 +77,17 @@ void cargarEmpleados(vector<empleado> &registro_empleado);
 void inicio();
 void menu_Inicio(vector<empleado> &registro_empleado);
 void imprimirMenuEmpleado(const empleado &empleado_activo);
+void imprimirMenuSubmenu();
+void menuEmpleado(empleado &empleado_activo);
+void SubmenuEmpleado(empleado &empleado_activo, vector<catalogo_ropa> &catalogo, vector<inventario_ropa> &inventario);
+void cargarCatalogo(vector<catalogo_ropa> &catalogo);
+void verCatalogo(vector<catalogo_ropa> &catalogo);
+void guardarCatalogo(vector<catalogo_ropa> &catalogo);
+void guardarInventario(vector<inventario_ropa> &catalogo);
+void verInventario(vector<inventario_ropa> &catalogo);
+void cargarInventario(vector<inventario_ropa> &catalogo);
+void comprarArticulos(vector<inventario_ropa> &inventario, vector<pedidos> &ventas, vector<catalogo_ropa> &catalogo);
+void colocarArticulos(vector<catalogo_ropa> &catalogo, vector<pedidos> &ventas, vector<inventario_ropa> &inventario);
 
 // Primer Menu
 void inicio() {
@@ -115,20 +143,20 @@ void registroEmpleado(vector<empleado> &registro_empleado) {
     cout << "\nIngrese su nombre: ";
     getline(cin, empleado_aux.nombre_empleado);
 
-    cout << "\nIngrese su apellido: ";
+    cout << "Ingrese su apellido: ";
     getline(cin, empleado_aux.apellido_empleado);
 
-    cout << "\nIngrese su edad: ";
+    cout << "Ingrese su edad: ";
     cin >> empleado_aux.edad;
 
     cin.ignore();
-    cout << "\nIngrese una contrasenia: ";
+    cout << "Ingrese una contrasenia: ";
     getline(cin, empleado_aux.contrasenia);
 
-    cout << "\nIngrese su correo: ";
+    cout << "Ingrese su correo: ";
     getline(cin, empleado_aux.correo);
 
-    cout << "\nIngrese su telefono (solo numeros): ";
+    cout << "Ingrese su telefono (solo numeros): ";
     getline(cin, empleado_aux.telefono);
 
     registro_empleado.push_back(empleado_aux);
@@ -189,21 +217,26 @@ void inicioSesionEmpleado(vector<empleado> &registro_empleado) {
     cout << "--------------------------------------------" << endl;
 
     string contrasenia;
-    cout << "\nIngrese su contrasenia: ";
-    cin.ignore();
-    getline(cin, contrasenia);
+    int intentos = 3;
 
-    bool encontrado = false;
-    for (const auto &emp : registro_empleado) {
-        if (emp.contrasenia == contrasenia) {
-            cout << "Bienvenido " << emp.nombre_empleado << endl;
-            encontrado = true;
-            break;
+    while (intentos > 0) {
+        cout << "\nIngrese su contrasenia (Intentos restantes: " << intentos << "): ";
+        cin.ignore();
+        getline(cin, contrasenia);
+
+        for (auto &empleado_activo : registro_empleado) {
+            if (empleado_activo.contrasenia == contrasenia) {
+                cout << "Bienvenido " << empleado_activo.nombre_empleado << "!" << endl;
+                menuEmpleado(empleado_activo);
+                return;
+            }
         }
+        cout << "Contraseña Incorrecta." << endl;
+        intentos--;
     }
 
-    if (!encontrado) {
-        cout << "Contraseña Incorrecta" << endl;
+    if (intentos == 0) {
+        cout << "Demasiados intentos fallidos. Regresando al menú principal." << endl;
     }
 }
 
@@ -215,21 +248,291 @@ void imprimirMenuEmpleado(const empleado &empleado_activo){
     cout << "\t  1.- Ver Perfil" << endl;
     cout << "\t  2.- Instrucciones" << endl;
     cout << "\t  3.- Planificacion antes de la jornada" << endl;
-    cout << "\t\t  3.1.- Ver catalogo" << endl;
-    cout << "\t\t  3.2.- Ver Stock" << endl;
-    cout << "\t\t  3.3.- Comprar Articulos" << endl;
-    cout << "\t\t  3.4.- Colocar Articulos" << endl;
-    cout << "\t\t  3.4.- Ver Historial de Compras" << endl;
-    cout << "\t  4.- Iniciar Jornada" << endl;
+    cout << "\t  4.- Ver Historial de Compras" << endl;
+    cout << "\t  5.- Iniciar Jornada" << endl;
+    cout << "\t  6.- Cerrar Sesion" << endl;
     cout << "--------------------------------------------" << endl;
     cout << "\n\n\nOpcion: ";
 }
+void imprimirMenuSubmenu() {
+    cout << "--------------------------------------------" << endl;
+    cout << "\tPlanificación antes de la jornada" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "\t  1.- Ver catálogo" << endl;
+    cout << "\t  2.- Ver Inventario" << endl;
+    cout << "\t  3.- Comprar artículos" << endl;
+    cout << "\t  4.- Colocar artículos en el catalogo" << endl;
+    cout << "\t  5.- Historial de pedidos" << endl;
+    cout << "\t  6.- Volver al menú principal" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "\n\n\nOpción: ";
+}
 
+void SubmenuEmpleado(empleado &empleado_activo, vector<catalogo_ropa> &catalogo, vector<inventario_ropa> &inventario) {
+    int opcion;
+    do {
+        imprimirMenuSubmenu();
+        cin >> opcion;
+        switch (opcion) {
+            case 1:
+                cout << "Ver catálogo" << endl;
+                verCatalogo(catalogo);
+                break;
+            case 2:
+                cout << "Ver Inventario" << endl;
+                verInventario(inventario);
+                break;
+            case 3:
+                cout << "Comprar artículos" << endl;
+                comprarArticulos(inventario, ventas, catalogo);
+                break;
+            case 4:
+                cout << "Colocar artículos" << endl;
+                // Agregar lógica específica aquí
+                break;
+            case 5:
+                cout << "Mis pedidos" << endl;
+                // Agregar lógica específica aquí
+                break;
+            case 6:
+                cout << "Volviendo al menú principal..." << endl;
+                break;
+            default:
+                cout << "Opción no válida." << endl;
+                break;
+        }
+    } while (opcion != 6);
+}
 
+void menuEmpleado(empleado &empleado_activo) {
+    int opcion;
+    do {
+        imprimirMenuEmpleado(empleado_activo);
+        cin >> opcion;
+        switch (opcion) {
+            case 1:
+                cout << "\n\n--------------------------------------------" << endl;
+                cout << "\t\tPerfil" << endl;
+                cout << "--------------------------------------------" << endl;
+                cout << "\t\tNombre: " << empleado_activo.nombre_empleado << endl;
+                cout << "\t\tApellido: " << empleado_activo.apellido_empleado << endl;
+                cout << "\t\tEdad: " << empleado_activo.edad << endl;
+                cout << "\t\tCorreo: " << empleado_activo.correo << endl;
+                cout << "\t\tTeléfono: " << empleado_activo.telefono << endl;
+                cout << "\n" << endl;
+                break;
+            case 2:
+                cout << "Instrucciones" << endl;
+                break;
+            case 3:
+                cout << "Planificación antes de la jornada" << endl;
+                SubmenuEmpleado(empleado_activo, catalogo, inventario); // Llamada a la función SubmenuEmpleado
+                break;
+            case 4:
+                cout << "Ver historial de compras" << endl;
+                break;
+            case 5:
+                cout << "Iniciar jornada" << endl;
+                break;
+            case 6:
+                cout << "Cerrando sesión..." << endl;
+                break;
+            default:
+                cout << "Opción no válida." << endl;
+                break;
+        }
+    } while (opcion != 6);
+}
+
+#include <fstream>
+#include <sstream>
+
+void verCatalogo(const vector<catalogo_ropa> &catalogo) {
+    cout << "\n--------------------------------------------" << endl;
+    cout << "\t\tVer catálogo" << endl;
+    cout << "--------------------------------------------" << endl;
+
+    if (catalogo.empty()) {
+        cout << "El catálogo se encuentra vacío." << endl;
+        return;
+    }
+
+    for (const auto &articulo : catalogo) {
+        cout << "Código: " << articulo.codigo_ropa << endl;
+        cout << "Categoría: " << articulo.categoria << endl;
+        cout << "Marca: " << articulo.marca << endl;
+        cout << "Talla: " << articulo.talla << endl;
+        cout << "Precio Unitario: $" << articulo.precio_unitario << endl;
+        cout << "Cantidad Disponible: " << articulo.cantidad << endl;
+        cout << "Temporada: " << articulo.temporada << endl;
+        cout << "--------------------------------------------" << endl;
+    }
+}
+
+void verInventario(const vector<inventario_ropa> &inventario) {
+    cout << "\n--------------------------------------------" << endl;
+    cout << "\t\tInventario" << endl;
+    cout << "--------------------------------------------" << endl;
+
+    if (inventario.empty()) {
+        cout << "El inventario se encuentra vacío." << endl;
+        return;
+    }
+
+    for (const auto &articulo : inventario) {
+        cout << "Código: " << articulo.codigo_ropa << endl;
+        cout << "Categoría: " << articulo.categoria << endl;
+        cout << "Marca: " << articulo.marca << endl;
+        cout << "Talla: " << articulo.talla << endl;
+        cout << "Costo Unitario: $" << articulo.costo_unitario << endl;
+        cout << "Cantidad en Stock: " << articulo.cantidad << endl;
+        cout << "Temporada: " << articulo.temporada << endl;
+        cout << "--------------------------------------------" << endl;        
+    }
+}
+
+// Leer catálogo desde archivo
+void cargarCatalogo(vector<catalogo_ropa> &catalogo) {
+    ifstream archivo_catalogo("catalogo.txt");
+    
+    if (!archivo_catalogo.is_open()) {
+        cout << "Error al abrir el archivo de catálogo." << endl;
+        return;
+    }
+
+    string linea;
+    while (getline(archivo_catalogo, linea)) {
+        stringstream ss(linea);
+        catalogo_ropa articulo;
+        string precio, cantidad;
+
+        getline(ss, articulo.codigo_ropa, ';');
+        getline(ss, articulo.categoria, ';');
+        getline(ss, articulo.marca, ';');
+        getline(ss, articulo.talla, ';');
+        getline(ss, precio, ';');
+        getline(ss, cantidad, ';');
+        getline(ss, articulo.temporada, ';');
+
+        articulo.precio_unitario = stod(precio);
+        articulo.cantidad = stoi(cantidad);
+
+        catalogo.push_back(articulo);
+    }
+    archivo_catalogo.close();
+}
+
+void guardarCatalogo(const vector<catalogo_ropa> &catalogo) {
+    ofstream archivo_catalogo("catalogo.txt");
+    if (!archivo_catalogo.is_open()) {
+        cout << "Error al abrir el archivo de catálogo." << endl;
+        return;
+    }
+
+    for (const auto &articulo : catalogo) {
+        archivo_catalogo << articulo.codigo_ropa << ";"
+                         << articulo.categoria << ";"
+                         << articulo.marca << ";"
+                         << articulo.talla << ";"
+                         << articulo.precio_unitario << ";"
+                         << articulo.cantidad << ";"
+                         << articulo.temporada << endl;
+    }
+    archivo_catalogo.close();
+    }
+
+// Leer inventario desde archivo
+void cargarInventario(vector<inventario_ropa> &inventario) {
+    ifstream archivo_inventario("inventario.txt");
+    if (!archivo_inventario.is_open()) {
+        cout << "Error al abrir el archivo de inventario." << endl;
+        return;
+    }
+
+    string linea;
+    while (getline(archivo_inventario, linea)) {
+        stringstream ss(linea);
+        inventario_ropa articulo;
+        string costo, cantidad;
+
+        getline(ss, articulo.codigo_ropa, ';');
+        getline(ss, articulo.categoria, ';');
+        getline(ss, articulo.marca, ';');
+        getline(ss, articulo.talla, ';');
+        getline(ss, costo, ';');
+        getline(ss, cantidad, ';');
+        getline(ss, articulo.temporada, ';');
+
+        articulo.costo_unitario = stod(costo);
+        articulo.cantidad = stoi(cantidad);
+
+        inventario.push_back(articulo);
+    }
+    archivo_inventario.close();
+}
+
+void guardarInventario(const vector<inventario_ropa> &inventario) {
+    ofstream archivo_inventario("inventario.txt");
+    if (!archivo_inventario.is_open()) {
+        cout << "Error al abrir el archivo de inventario." << endl;
+        return;
+    }
+
+    for (const auto &articulo : inventario) {
+        archivo_inventario << articulo.codigo_ropa << ";"
+                           << articulo.categoria << ";"
+                           << articulo.marca << ";"
+                           << articulo.talla << ";"
+                           << articulo.costo_unitario << ";"
+                           << articulo.cantidad << ";"
+                           << articulo.temporada << endl;
+    }
+    archivo_inventario.close();
+    }
+void comprarArticulos(vector<inventario_ropa> &inventario, vector<pedidos> &ventas, vector<catalogo_ropa> &catalogo){
+    pedidos nuevo_pedido;
+
+    cout << "--------------------------------------------" << endl;
+    cout << "Comprar artículos" << endl;
+    cout << "--------------------------------------------" << endl;
+
+    cout << "Ingrese el código de la prenda que desea comprar: " << endl;
+    cin.ignore();
+    getline(cin, nuevo_pedido.codigo_pedido);
+
+    for (int i = 0; i < catalogo.size(); i++) {
+        if (catalogo[i].codigo_ropa == nuevo_pedido.codigo_pedido) {
+            cout << "Ingrese la cantidad de prendas que desea comprar: " << endl;
+            cin >> nuevo_pedido.cantidad;
+
+            nuevo_pedido.total = catalogo[i].precio_unitario * nuevo_pedido.cantidad;
+            ventas.push_back(nuevo_pedido);
+
+            inventario[i].cantidad += nuevo_pedido.cantidad;
+
+            guardarInventario(inventario);
+
+            ofstream archivo_pedidos("pedidos.txt", ios::app);
+            if (archivo_pedidos.is_open()) {
+                archivo_pedidos << nuevo_pedido.codigo_pedido << ";"
+                                << nuevo_pedido.cantidad << ";"
+                                << nuevo_pedido.total << endl;
+                archivo_pedidos.close();
+            }
+            cout << "Artículos comprados exitosamente." << endl;
+            return;
+        } else {
+            cout << "Prenda no encontrada en el catálogo." << endl;
+            return;
+        }
+    }
+}
 
 // Función principal
 int main() {
     cargarEmpleados(registro_empleado);
+    cargarInventario(inventario);
+    cargarCatalogo(catalogo);
     menu_Inicio(registro_empleado);
     return 0;
 }
